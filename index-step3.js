4004eef9a6c781f643491d79bb427323570da955
Axel <lezinaxel@gmail.com>
2016-10-05 15:57:46 +0200

    Les quatre étapes du nombre mystère ( sans factorisation )

---
+++ index-step3.js
@@ -0,0 +1,125 @@
+$(document).ready(main);
+
+// Fonction principale
+function main(){
+
+	// Créer et initialiser une variable 'globale' qui 
+	// va stocker le nombre de tentatives restantes.
+	var resultat = prompt('Choisissez votre nombre de vies:','');
+	$(resultat).val();
+	var vies = resultat;
+	$("#vies").text(vies);
+
+	function choice(min,max){
+	var min = parseInt(prompt('Choissisez le nombre minimum du nombre mystère:',''));
+	console.log(min);
+	console.log(typeof(min));
+	var max = parseInt(prompt('Choisissez le nombre maximum du nombre mystère:',''));
+	console.log(max);
+	var nombreMystere = Math.floor((max-min+1)*Math.random())+min
+	console.log(nombreMystere);
+
+
+}
+
+choice();
+
+	//var intervalle = prompt('Choisissez votre aire de jeu:','');
+	//Math.floor((max-min+1)*Math.random())+min	
+
+	// Créer et initialiser une variable 'globale' qui 
+	// va stocker le nombre "aléatoire" mystère.
+	//var rando = Math.floor(Math.random()*151);
+
+	// == Fonction clickValider == 
+	// Créer la fonction 'clickValider'
+	function clickValider(){
+		var nombre = parseInt($('#input').val(),10);
+		if(rando == nombre){
+			alert('Gagné !')
+
+		}
+
+		else if(nombre > rando){
+			alert('Perdu, votre nombre est trop grand');
+			console.log(vies--);
+			$('#vies').text(vies);
+
+		}else{
+			alert('Perdu, votre nombre est trop petit');
+			console.log(vies--);
+			$('#vies').text(vies);
+		}
+
+		if(vies == 0){
+			alert('T\' es un GROS NOOB')
+		}
+
+		if(rando == nombre || vies == 0){
+			var resultat = prompt('Choisissez votre nombre de vies:','');
+			vies = resultat;
+			rando = Math.floor(Math.random()*151);
+			$("#vies").text(vies);
+
+		}
+		
+		
+	};
+
+	$('#button').click(function(){
+		clickValider();
+	});
+}
+
+
+
+
+
+
+
+
+
+		// Récupérer le contenu de mon input
+
+		// Comparer ce contenu au nombre mystère.
+			/*
+				Si le contenu est égal : 
+				- afficher une alert Gagné
+				- démarrer une nouvelle partie
+				*/
+
+			// Sinon s'il est supérieur
+			// Afficher une alert Perdu, votre nombre est trop grand
+
+			// Sinon s'il est inférieur
+			// Afficher une alert Perdu, votre nombre est trop petit
+
+			// Dans les cas où c'est perdu, diminuer le nombre de tentatives
+
+			// Actualiser la zone html affichant le nombre de tentatives
+
+			/* 
+				Si le nb de tentatives est égal à 0,
+			 	la partie est perdue :
+			 	- afficher que c'est perdu
+			 	- démarrer une nouvelle partie
+			 		+ remettre le nombre de tentatives à sa valeur initiale
+			 		+ générer un nouveau nombre mystère
+			 		*/
+
+	// /Fin == Fonction clickValider == 
+
+
+
+	// Ecrire le code qui va détecter que le bouton 'valider' est cliqué.
+	// Et qui appelle la fonction 'clickValider' défini ci-dessus
+
+
+	/*
+		Concernant la 'refactorisation' : 
+		- créer une fonction 'demarrerPartie'
+		- créer une fonction 'partieGagnee'
+		- créer une fonction 'partiePerdue'
+		*/
+
+
