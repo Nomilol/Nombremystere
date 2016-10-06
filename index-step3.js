1f5f63d97d74d6513ee71f6313a90d4180cb0b7d
Axel <lezinaxel@gmail.com>
2016-10-06 16:12:24 +0200

    Code factorisé pour le nombre mystère

--- index-step3.js
+++ index-step3.js
@@ -1,75 +1,69 @@
 $(document).ready(main);
 
-// Fonction principale
 function main(){
 
-	// Créer et initialiser une variable 'globale' qui 
-	// va stocker le nombre de tentatives restantes.
+	
 	var vies;
 	$(vies).val();
 	$("#vies").text(vies);
-	var min;
-	var max ;
 	var nombreMystere; 
-	var nombre;
 	var resultat;
-	//console.log(nombreMystere);
 
-function demarrerPartie(){ // Ne pas oublier d'apeller la fonction
-vie = prompt('Choisissez votre nombre de vies:','');
-min = parseInt(prompt('Choissisez le nombre minimum du nombre mystère:',''));
-max = parseInt(prompt('Choisissez le nombre maximum du nombre mystère:',''));
-nombreMystere = Math.floor((max-min+1)*Math.random())+min
-$("#vies").text(vies);
-console.log(nombreMystere);
-}
+	function demarrerPartie(){ 
+
+		vies = parseInt(prompt('Choisissez votre nombre de vies:'),10);
+		var min = parseInt(prompt('Choissisez le nombre minimum du nombre mystère:'),10);
+		var max = parseInt(prompt('Choisissez le nombre maximum du nombre mystère:'),10);
+		nombreMystere = Math.floor((max-min+1)*Math.random())+min
+		$("#vies").text(vies);
+		console.log(nombreMystere);
+	}
+
+	function clickValider(){
+		var userNombre = parseInt($('input').val(),10);
+		//debugger
+
+		if( userNombre > nombreMystere){
+			alert('Perdu, votre nombre est trop grand');
+			console.log(vies--);
+			$('#vies').text(vies);
+
+		}
+
+		else if(userNombre < nombreMystere){
+			alert('Perdu, votre nombre est trop petit');
+			console.log(vies--);
+			$('#vies').text(vies);
+		}
+		else if (userNombre === nombreMystere){
+			partieGagnee();
 
 
+		}
+
+		if(nombreMystere === userNombre || vies === 0){
+			demarrerPartie();
+		}
+
+	}
 
 
-
-function clickValider(){
-	if(partieGagnee()){
-	};
+	function partieGagnee(){
+		alert('GG GROS');
 
 
-if(nombre > nombreMystere){
-	alert('Perdu, votre nombre est trop grand');
-	console.log(vies--);
-	$('#vies').text(vies);
-
-}
-
-else if(nombre < nombreMystere){
-	alert('Perdu, votre nombre est trop petit');
-	console.log(vies--);
-	$('#vies').text(vies);
-}
-
-if(nombreMystere == nombre || vies == 0){
-	demarrerPartie();
-}
-
-function partieGagnee(){
-	nombre = parseInt($('#input').val(),10);
-	return nombreMystere == nombre
-	alert('GG GROS');
+	}
 
 	function partiePerdue(){
-		if(vies == 0){
-			alert('T\' es un GROS NOOB')
-		};
-	}
-}
+		alert('T\'es un GROS NOOB');
+		vies === 0;
+		demarrerPartie();
+	};
 
-}
-
-demarrerPartie();
-
-
-$('#button').click(function(){
-	clickValider();
-});
+	demarrerPartie();
+	$('#button').click(function(){
+		clickValider();
+	});
 }
 
 
@@ -80,10 +74,6 @@ $('#button').click(function(){
 
 
 
-} // Ne pas oublier d'apeller la fonction
-
-
- // Ne pas oublier d'appeller la fonction
 
 
 
@@ -95,66 +85,5 @@ $('#button').click(function(){
 
 
 
-
-	//Math.floor((max-min+1)*Math.random())+min	
-
-	// Créer et initialiser une variable 'globale' qui 
-	// va stocker le nombre "aléatoire" mystère.
-	//var rando = Math.floor(Math.random()*151);
-
-	// == Fonction clickValider == 
-	// Créer la fonction 'clickValider'
-
-
-
-
-
-
-
-
-
-
-		// Récupérer le contenu de mon input
-
-		// Comparer ce contenu au nombre mystère.
-			/*
-				Si le contenu est égal : 
-				- afficher une alert Gagné
-				- démarrer une nouvelle partie
-				*/
-
-			// Sinon s'il est supérieur
-			// Afficher une alert Perdu, votre nombre est trop grand
-
-			// Sinon s'il est inférieur
-			// Afficher une alert Perdu, votre nombre est trop petit
-
-			// Dans les cas où c'est perdu, diminuer le nombre de tentatives
-
-			// Actualiser la zone html affichant le nombre de tentatives
-
-			/* 
-				Si le nb de tentatives est égal à 0,
-			 	la partie est perdue :
-			 	- afficher que c'est perdu
-			 	- démarrer une nouvelle partie
-			 		+ remettre le nombre de tentatives à sa valeur initiale
-			 		+ générer un nouveau nombre mystère
-			 		*/
-
-	// /Fin == Fonction clickValider == 
-
-
-
-	// Ecrire le code qui va détecter que le bouton 'valider' est cliqué.
-	// Et qui appelle la fonction 'clickValider' défini ci-dessus
-
-
-	/*
-		Concernant la 'refactorisation' : 
-		- créer une fonction 'demarrerPartie'
-		- créer une fonction 'partieGagnee'
-		- créer une fonction 'partiePerdue'
-		*/
 
 
